ea7d396cc9136ae270b754ffafe1b8c9115fbb21
Liam Dubay <liam.dubay@gmail.com>
2022-06-29 15:31:57 -0400

    removed matplotlib settings

--- migration/src/plots/env.py
+++ migration/src/plots/env.py
@@ -37,27 +37,27 @@ else: pass
 # by setting matplotlib rcParams here, all plots will be automatically
 # formatted since this file will be imported everywhere to ensure dependencies
 # are met anyway.
-mpl.rcParams["font.family"] = "serif"
-mpl.rcParams["text.usetex"] = True
-mpl.rcParams["text.latex.preamble"] = [r"\usepackage{amsmath}"]
-mpl.rcParams["errorbar.capsize"] = 5
-mpl.rcParams["axes.linewidth"] = 2
-mpl.rcParams["xtick.major.size"] = 16
-mpl.rcParams["xtick.major.width"] = 2
-mpl.rcParams["xtick.minor.size"] = 8
-mpl.rcParams["xtick.minor.width"] = 1
-mpl.rcParams["ytick.major.size"] = 16
-mpl.rcParams["ytick.major.width"] = 2
-mpl.rcParams["ytick.minor.size"] = 8
-mpl.rcParams["ytick.minor.width"] = 1
-mpl.rcParams["axes.labelsize"] = 30
-mpl.rcParams["xtick.labelsize"] = 25
-mpl.rcParams["ytick.labelsize"] = 25
-mpl.rcParams["legend.fontsize"] = 25
-mpl.rcParams["xtick.direction"] = "in"
-mpl.rcParams["ytick.direction"] = "in"
-mpl.rcParams["ytick.right"] = True
-mpl.rcParams["xtick.top"] = True
-mpl.rcParams["xtick.minor.visible"] = True
-mpl.rcParams["ytick.minor.visible"] = True
+# mpl.rcParams["font.family"] = "serif"
+# mpl.rcParams["text.usetex"] = True
+# mpl.rcParams["text.latex.preamble"] = [r"\usepackage{amsmath}"]
+# mpl.rcParams["errorbar.capsize"] = 5
+# mpl.rcParams["axes.linewidth"] = 2
+# mpl.rcParams["xtick.major.size"] = 16
+# mpl.rcParams["xtick.major.width"] = 2
+# mpl.rcParams["xtick.minor.size"] = 8
+# mpl.rcParams["xtick.minor.width"] = 1
+# mpl.rcParams["ytick.major.size"] = 16
+# mpl.rcParams["ytick.major.width"] = 2
+# mpl.rcParams["ytick.minor.size"] = 8
+# mpl.rcParams["ytick.minor.width"] = 1
+# mpl.rcParams["axes.labelsize"] = 30
+# mpl.rcParams["xtick.labelsize"] = 25
+# mpl.rcParams["ytick.labelsize"] = 25
+# mpl.rcParams["legend.fontsize"] = 25
+# mpl.rcParams["xtick.direction"] = "in"
+# mpl.rcParams["ytick.direction"] = "in"
+# mpl.rcParams["ytick.right"] = True
+# mpl.rcParams["xtick.top"] = True
+# mpl.rcParams["xtick.minor.visible"] = True
+# mpl.rcParams["ytick.minor.visible"] = True
 
