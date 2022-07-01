6bbcd0821feaea766336bc190c021585ac88bba3
Liam Dubay <liam.dubay@gmail.com>
2022-07-01 15:04:34 -0400

    Update run_all.sh

--- migration/run_all.sh
+++ migration/run_all.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 for DTD in "powerlaw" "powerlaw_steep" "powerlaw_broken" "exponential" "exponential_long" "bimodal"
 do
-	for MIGR in "diffusion" "post-process"
+	for EVOL in "insideout" "lateburst" "conroy22"
 	do
-		NAME=outputs/$MIGR/insideout/$DTD
-		echo $NAME
-		python simulations.py -f --nstars=8 --migration=$MIGR --evolution=insideout --RIa=$DTD --name=$NAME
+		for MIGR in "diffusion" "post-process"
+		do
+			NAME=outputs/$MIGR/$EVOL/$DTD
+			echo $NAME
+			python simulations.py -f --nstars=8 --migration=$MIGR --evolution=$EVOL --RIa=$DTD --name=$NAME
+		done
 	done
 done
 for DTD in "powerlaw" "powerlaw_steep" "exponential"
 do
-	for MIGR in "diffusion" "post-process"
+	for EVOL in "insideout" "lateburst" "conroy22"
 	do
-		NAME=outputs/$MIGR/insideout/$DTD\_delayed
-		echo $NAME
-		python simulations.py -f --nstars=8 --migration=$MIGR --evolution=insideout --RIa=$DTD --minimum-delay=0.15 --name=$NAME
+		for MIGR in "diffusion" "post-process"
+		do
+			NAME=outputs/$MIGR/$EVOL/$DTD\_delayed
+			echo $NAME
+			python simulations.py -f --nstars=8 --migration=$MIGR --evolution=$EVOL --RIa=$DTD --minimum-delay=0.15 --name=$NAME
+		done
 	done
 done
