913f1b4ec329f95ed4d3f2cd385744608e669dd7
Liam Dubay <liam.dubay@gmail.com>
2022-07-11 12:50:59 -0400

    added alternate onezone plot

---
+++ src/scripts/onezone_delay_taustar_2.py
@@ -0,0 +1,119 @@
+"""
+This script plots abundance tracks for one-zone models with varying minimum
+Type Ia delay times and star formation efficiency timescales.
+"""
+
+import sys
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+import vice
+from vice.yields.presets import JW20
+vice.yields.sneia.settings['fe'] *= 10**0.1
+import paths
+sys.path.append(str(paths.root))
+from migration.src.simulations import models, dtds
+from migration.src._globals import END_TIME
+from colormaps import paultol
+from tracks_and_distributions import setup_axes, plot_vice_onezone
+
+# Settings
+MINIMUM_DELAY = [0.08, 0.04, 0.16, 0.08, 0.08] # Gyr
+TAU_STAR = [2.0, 2.0, 2.0, 1.0, 4.0] # Gyr
+LINE_STYLE = ['-', ':', '--', '-', '-']
+COLOR = ['k', 'k', 'k', paultol.highcontrast.colors[2],
+         paultol.highcontrast.colors[1]]
+NRUNS = len(MINIMUM_DELAY)
+DT = 0.01
+STANDARD_PARAMS = dict(
+    func=models.insideout(8, dt=DT),
+    mode='sfr',
+    RIa=dtds.powerlaw(),
+    elements=('fe', 'o'),
+    dt=DT,
+    recycling='continuous',
+    eta=2.5,
+)
+
+def main(overwrite=False):
+    output_dir = paths.data / 'onezone' / 'delay_taustar'
+
+    fig, axs = setup_axes(feh_lim=(-3.1, 0.1), ofe_lim=(-0.16, 0.52))
+
+    for i in range(NRUNS):
+        delay = MINIMUM_DELAY[i]
+        tau_star = TAU_STAR[i]
+        name = gen_name_from_params(delay=delay, tau_star=tau_star)
+        label = rf'$t_D={int(delay*1000)}$ Myr, $\tau_*={int(tau_star)}$ Gyr'
+
+        if overwrite:
+            run(output_dir, i)
+        else:
+            try:
+                history = vice.history(str(output_dir / name))
+            except IOError:
+                run(output_dir, i)
+
+        if tau_star != 2.0:
+            line_width = 1.5
+            zorder = 1
+        else:
+            line_width = 1
+            zorder = 10
+
+        plot_vice_onezone(str(output_dir / name), fig=fig, axs=axs, label=label,
+                          color=COLOR[i], linestyle=LINE_STYLE[i],
+                          linewidth=line_width, zorder=zorder)
+
+    axs[0].legend(frameon=False, loc='lower left', bbox_to_anchor=(0.12, 0.1))
+    fig.savefig(paths.figures / 'onezone_delay_taustar_2.png', dpi=300)
+    plt.close()
+
+
+def run(output_dir, i):
+    """
+    Set up and run the ith one-zone model.
+    """
+    sz = setup_single(output_dir,
+                      delay=MINIMUM_DELAY[i], tau_star=TAU_STAR[i],
+                      **STANDARD_PARAMS)
+    simtime = np.arange(0, END_TIME + DT, DT)
+    sz.run(simtime, overwrite=True)
+
+
+def setup_single(output_dir, delay=0.1, tau_star=2., **kwargs):
+    """
+    Setup a one-zone model with a given minimum Ia delay time and SFE timescale.
+
+    Parameters
+    ----------
+    output_dir : Path
+        Parent directory for VICE outputs
+    delay : float, optional
+        Minimum Type Ia delay time in Gyr. The default is 0.1 Gyr.
+    tau_star : float, optional
+        Star formation efficiency timescale in Gyr. The default is 2 Gyr.
+    Other keyword arguments are passed to vice.singlezone
+
+    Returns
+    -------
+    sz : vice.singlezone object
+    """
+    name = gen_name_from_params(delay=delay, tau_star=tau_star)
+    sz = vice.singlezone(name=str(output_dir / name),
+                         delay=delay, tau_star=tau_star,
+                         **kwargs)
+    return sz
+
+
+def gen_name_from_params(delay=0.1, tau_star=2.0):
+    """
+    Generate singlezone output name from its distinguishing parameters.
+    """
+    name = 'delay{:03d}_taustar{:02d}'.format(int(delay*1000), int(tau_star*10))
+    return name
+
+
+
+if __name__ == '__main__':
+    main()
--- src/scripts/tracks_and_distributions.py
+++ src/scripts/tracks_and_distributions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
 
 def main():
@@ -10,9 +11,23 @@ def main():
     from migration.src.simulations import models, dtds
     from vice.yields.presets import JW20
 
-    fname = str(paths.data / 'onezone' / 'test')
+    fname = str(paths.data / 'onezone' / 'test01')
     sz = vice.singlezone(name=fname, func=models.insideout(8), mode='sfr',
-                         RIa=dtds.powerlaw())
+                         RIa=dtds.powerlaw(), delay=0.04)
+    simtime = np.arange(0, 13.21, 0.01)
+    sz.run(simtime, overwrite=True)
+    hist = vice.history(fname)
+    mdf = vice.mdf(fname)
+    mdf_bins = mdf['bin_edge_left'] + mdf['bin_edge_right'][-1:]
+    fig, axs = plot_track_and_mdf(hist['[fe/h]'], hist['[o/fe]'],
+                                  dn_dfeh=mdf['dn/d[fe/h]'], feh_bins=mdf_bins,
+                                  dn_dofe=mdf['dn/d[o/fe]'], ofe_bins=mdf_bins,
+                                  feh_lim=(-2.8, 0.), ofe_lim=(-0.1, 0.5),
+                                  filled=True)
+
+    fname = str(paths.data / 'onezone' / 'test02')
+    sz = vice.singlezone(name=fname, func=models.insideout(8), mode='sfr',
+                         RIa=dtds.bimodal(), delay=0.04)
     simtime = np.arange(0, 13.21, 0.01)
     sz.run(simtime, overwrite=True)
     hist = vice.history(fname)
@@ -20,27 +35,48 @@ def main():
     mdf_bins = mdf['bin_edge_left'] + mdf['bin_edge_right'][-1:]
     plot_track_and_mdf(hist['[fe/h]'], hist['[o/fe]'],
                        dn_dfeh=mdf['dn/d[fe/h]'], feh_bins=mdf_bins,
-                       dn_dofe=mdf['dn/d[o/fe]'], ofe_bins=mdf_bins)
+                       dn_dofe=mdf['dn/d[o/fe]'], ofe_bins=mdf_bins,
+                       fig=fig, axs=axs,
+                       feh_lim=(-2.8, 0.), ofe_lim=(-0.1, 0.5),
+                       filled=False, color='r')
+
     plt.savefig(paths.figures / 'track_dist_test.png', dpi=300)
     plt.close()
 
 
+def plot_vice_onezone(output, fig=None, axs=[], **kwargs):
+    import vice
+    hist = vice.history(output)
+    mdf = vice.mdf(output)
+    mdf_bins = mdf['bin_edge_left'] + mdf['bin_edge_right'][-1:]
+    fig, axs = plot_track_and_mdf(hist['[fe/h]'], hist['[o/fe]'],
+                       dn_dfeh=mdf['dn/d[fe/h]'], feh_bins=mdf_bins,
+                       dn_dofe=mdf['dn/d[o/fe]'], ofe_bins=mdf_bins,
+                       fig=fig, axs=axs, **kwargs)
+    return fig, axs
+
+
+
 def plot_track_and_mdf(feh, ofe, dn_dfeh=[], feh_bins=[], dn_dofe=[], ofe_bins=[],
-                       fig=None, axs=[], color='k', linestyle='-', filled=False):
+                       fig=None, axs=[], color='k', linestyle='-',
+                       linewidth=1, zorder=10, filled=False, label=None,
+                       **kwargs):
     if fig == None:
-        fig, axs = setup_axes()
+        fig, axs = setup_axes(**kwargs)
     if filled:
         histtype = 'stepfilled'
     else:
         histtype = 'step'
-    axs[0].plot(feh, ofe, color=color, linestyle=linestyle, linewidth=1)
+    axs[0].plot(feh, ofe, color=color, linestyle=linestyle, linewidth=linewidth,
+                zorder=zorder, label=label)
     axs[1].hist(feh_bins[:-1], feh_bins, weights=dn_dfeh,
                 histtype=histtype, color=color, linestyle=linestyle,
-                linewidth=1)
+                linewidth=linewidth, zorder=zorder)
     axs[2].hist(ofe_bins[:-1], ofe_bins, weights=dn_dofe,
                 orientation='horizontal',
                 histtype=histtype, color=color, linestyle=linestyle,
-                linewidth=1)
+                linewidth=linewidth, zorder=zorder)
+    return fig, axs
 
 
 def setup_axes(feh_lim=(-3, 1), ofe_lim=(-0.2, 0.6), width=3.25, **kwargs):
@@ -64,22 +100,41 @@ def setup_axes(feh_lim=(-3, 1), ofe_lim=(-0.2, 0.6), width=3.25, **kwargs):
     axs : list of matplotlib.axes.Axes
     """
     # Start with the center panel
-    fig, ax_main = plt.subplots(figsize=(width, width))
-    fig.subplots_adjust(left=0.3, bottom=0.25, right=0.95, top=0.95)
+    fig, ax_main = plt.subplots(figsize=(width, width), **kwargs)
+    # fig.subplots_adjust(left=0.3, bottom=0.25, right=0.95, top=0.95)
+    fig.subplots_adjust(left=0.15, bottom=0.12, right=0.9, top=0.95)
+    ax_main.spines['top'].set_visible(False)
+    ax_main.spines['right'].set_visible(False)
+    ax_main.tick_params(top=False, right=False, which='both')
+    ax_main.tick_params(direction='out', which='both')
+    ax_main.xaxis.set_minor_locator(MultipleLocator(0.1))
+    ax_main.yaxis.set_minor_locator(MultipleLocator(0.02))
     ax_main.set_xlim(feh_lim)
     ax_main.set_ylim(ofe_lim)
+    ax_main.set_xlabel('[Fe/H]')
+    ax_main.set_ylabel('[O/Fe]')
     # Add panel below for MDF
-    ax_mdf = plt.axes([0.3, 0.1, 0.65, 0.12], sharex=ax_main)
-    ax_mdf.set_xlabel('[Fe/H]')
+    # ax_mdf = plt.axes([0.3, 0.1, 0.65, 0.12], sharex=ax_main)
+    ax_mdf = plt.axes([0.15, 0.12, 0.75, 0.1], sharex=ax_main)
     ax_mdf.set_yscale('log')
-    ax_mdf.tick_params(left=False, right=False, which='minor')
-    # ax_main.set_xticklabels([])
+    # ax_mdf.spines['top'].set_visible(False)
+    # ax_mdf.spines['bottom'].set_visible(False)
+    # ax_mdf.spines['left'].set_visible(False)
+    # ax_mdf.spines['right'].set_visible(False)
+    # ax_mdf.tick_params(top=False, left=False, bottom=False, right=True,
+    #                    which='both', direction='out')
+    # ax_mdf.minorticks_off()
+    # ax_mdf.tick_params(axis='x', labelcolor='#ffffff00')
+    # ax_mdf.tick_params(axis='y', labelsize=6)
+    # ax_mdf.yaxis.set_label_position('right')
+    # ax_mdf.yaxis.tick_right()
+    # ax_mdf.set_ylim((0.0001, 10))
+    ax_mdf.axis('off')
     # Add panel to the left for MDF in [O/Fe]
-    ax_odf = plt.axes([0.15, 0.25, 0.12, 0.7], sharey=ax_main)
-    ax_odf.set_ylabel('[O/Fe]')
+    # ax_odf = plt.axes([0.15, 0.25, 0.12, 0.7], sharey=ax_main)
+    ax_odf = plt.axes([0.15, 0.12, 0.1, 0.83], sharey=ax_main)
     ax_odf.set_xscale('log')
-    ax_odf.tick_params(top=False, bottom=False, which='minor')
-    # ax_main.set_yticklabels([])
+    ax_odf.axis('off')
     axs = [ax_main, ax_mdf, ax_odf]
     return fig, axs
 
