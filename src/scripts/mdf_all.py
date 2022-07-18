d45a9ac2b152c15a2c1b1c0ecaac05b5ff595353
Liam Dubay <liam.dubay@gmail.com>
2022-07-18 11:11:38 -0400

    reverted to 9 panel format

--- src/scripts/mdf_all.py
+++ src/scripts/mdf_all.py
@@ -2,7 +2,7 @@
 Plot MDFs from all multizone runs
 """
 
-from mdf import main
+from mdf_9panel import main
 
 for evolution in ['conroy22', 'insideout', 'lateburst']:
     for RIa in ['powerlaw', 'powerlaw_delayed', 'powerlaw_steep',
