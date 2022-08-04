193ffaeb46fd036facf0af4a30dcab3dca852a58
Liam Dubay <liam.dubay@gmail.com>
2022-08-04 12:56:51 -0400

    Update __init__.py

--- migration/src/simulations/dtds/__init__.py
+++ migration/src/simulations/dtds/__init__.py
@@ -1,10 +1,11 @@
 
-__all__ = ["powerlaw", "exponential", "prompt", "powerlaw_broken",
+__all__ = ["powerlaw", "exponential", "prompt", "broken_powerlaw", "plateau",
            "greggio05_single", "greggio05_double", "greggio05_approximate"]
 from .powerlaw import powerlaw
 from .exponential import exponential
 from .prompt import prompt
-from .powerlaw_broken import powerlaw_broken
+from .broken_powerlaw import broken_powerlaw
+from .plateau import plateau
 from .greggio05_single import greggio05_single
 from .greggio05_double import greggio05_double
 from .greggio05_approximate import greggio05_approximate
