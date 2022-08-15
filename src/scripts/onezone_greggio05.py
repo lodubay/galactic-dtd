9e81b9f6b9977d079f89caf1a5f95e31a5c0f363
Liam Dubay <liam.dubay@gmail.com>
2022-08-15 11:42:16 -0400

    Update onezone_greggio05.py

--- src/scripts/onezone_greggio05.py
+++ src/scripts/onezone_greggio05.py
@@ -4,8 +4,8 @@ degenerate cases from Grettio 2005. Reference this script in the `\script{}`
 command when plotting both figures side-by-side in the LaTeX document.
 """
 
-from .onezone_greggio05_single import main as main_single
-from .onezone_greggio05_double import main as main_double
+from onezone_greggio05_single import main as main_single
+from onezone_greggio05_double import main as main_double
 
 main_single()
 main_double()
