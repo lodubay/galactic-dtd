7706ae6be89b520cdaae35dff2f040fac145ce84
Liam Dubay <liam.dubay@gmail.com>
2023-02-06 08:53:34 -0500

    add triple evolution dtd

--- src/scripts/ofe_feh_all.py
+++ src/scripts/ofe_feh_all.py
@@ -7,7 +7,8 @@ for evolution in ['insideout', 'lateburst', 'conroy22', 'twoinfall']:
                 'exponential_timescale30',
                 'plateau_width300_slope11',
                 'plateau_width1000_slope11',
-                'prompt_peak050_stdev015_timescale30']:
+                'prompt_peak050_stdev015_timescale30',
+                'triple_delay040']:
         output_name = '/'.join(('diffusion', evolution, RIa))
         print('Plotting %s' % output_name)
         main(output_name)
