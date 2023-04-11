99a1a1b6387f01a91fc58e9dc9aea413f20dd074
Liam Dubay <liam.dubay@gmail.com>
2023-04-11 11:30:14 -0400

    revert bimodality test to two-region fit

--- src/scripts/bimodality_test.py
+++ src/scripts/bimodality_test.py
@@ -8,9 +8,9 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
-from scipy.stats import norm, skewnorm, chisquare
+from scipy.stats import norm
 import matplotlib.pyplot as plt
-from utils import multioutput_to_pandas, get_bin_centers, kl_divergence
+from utils import multioutput_to_pandas, get_bin_centers
 from feh_distribution import vice_mdf
 import paths
 
@@ -35,7 +35,7 @@ DTDs = ['powerlaw_slope11',
 def main():
     summary_table = pd.DataFrame([], 
         index=pd.MultiIndex.from_product([DTDs, SFHs], names=['DTD', 'SFH']),
-        columns=['DeltaChi2', 'KLDiv', 'DeltaMu', 'LowAlphaCoeff'],
+        columns=['Significance'],
     )
     
     with tqdm(total=len(SFHs) * len(DTDs)) as t:
@@ -46,17 +46,19 @@ def main():
                 t.update()
     
     print(summary_table)
-    summary_table.to_csv(paths.figures / 'bimodality/skewnorm/bimodality.csv')
+    summary_table.to_csv(paths.figures / 'bimodality/tworegions/bimodality.csv')
     
 
-def bimodality(output_name, diagnostic=True, galr_lim=(5, 7), absz_lim=(0.5, 1)):
+def bimodality(output_name, diagnostic=True, inner_galr=(3,5), inner_absz=(1,2),
+               outer_galr=(13,15), outer_absz=(0,0.5)):
     """
     Determine if the [O/Fe] distribution of the given VICE output is bimodal.
     
-    This function fits a single skewed Gaussian and a model with two skewed
-    Gaussians to the [O/Fe] distribution in a galactic region known to show
-    alpha-element bimodality. If the two-Gaussian fit has a lower chi-square
-    statistic than the one-Gaussian fit, the distribution is bimodal.
+    This function fits a Gaussian to the [O/Fe] distribution in two galactic
+    regions: an extreme inner and extreme outer region, both of which should
+    be far from the midplane to maximize the fraction of high-alpha stars.
+    If the Gaussian fits are substantially different, the distribution is
+    bimodal.
     
     Parameters
     ----------
@@ -66,181 +68,68 @@ def bimodality(output_name, diagnostic=True, galr_lim=(5, 7), absz_lim=(0.5, 1))
     diagnostic : bool, optional
         If True, make a diagnostic plot illustrating the inner and outer ODFs
         and their respective fits. The default is True.
-    galr_lim : tuple, optional
-        Bounds on the galactocentric radius in kpc. The default is (5, 7).
-    absz_lim : tuple, optional
-        Bounds on the distance from the midplane in kpc. The default is (0.5, 1).
+    inner_galr : tuple, optional
+        Limits on Rgal of the inner region in kpc. Should be close to the 
+        center to select the high-alpha sequence. The default is (3, 5).
+    inner_absz : tuple, optional
+        Limits on |z| of the inner region in kpc. Should be far from the
+        midplane to select the high-alpha sequence. The default is (1, 2).
+    outer_galr : tuple, optional
+        Limits on Rgal of the outer region in kpc. Should be far from the 
+        center to select the low-alpha sequence. The default is (13, 15).
+    outer_absz : tuple, optional
+        Limits on |z| of the outer region in kpc. Should be close to the 
+        midplane to select the low-alpha sequence. The default is (0, 0.5).
     
     Returns
     -------
-    delta_chisq : float
-        Difference in the chi-square statistic between the one- and two-Gaussian
-        fits. If this is positive, the distribution is bimodal.
+    delta_mean : float
+        Difference between the means of the inner and outer region fits
+        divided by the RMS standard deviation in data units.
     """
     vice_stars = multioutput_to_pandas(output_name)
-    # galactic region ODF
-    odf, bin_edges = vice_mdf(vice_stars, col='[o/fe]', 
-                              galr_lim=galr_lim, absz_lim=absz_lim, 
-                              xlim=OFE_LIM, bin_width=BIN_WIDTH, 
-                              smoothing=False)
-    dx = bin_edges[1] - bin_edges[0]
+    # inner region ODF
+    odf_inner, bin_edges = vice_mdf(vice_stars, col='[o/fe]', 
+                                    galr_lim=inner_galr, absz_lim=inner_absz, 
+                                    xlim=OFE_LIM, bin_width=BIN_WIDTH, 
+                                    smoothing=False)
+    # outer region ODF
+    odf_outer, bin_edges = vice_mdf(vice_stars, col='[o/fe]', 
+                                    galr_lim=outer_galr, absz_lim=outer_absz, 
+                                    xlim=OFE_LIM, bin_width=BIN_WIDTH, 
+                                    smoothing=False)
     ofe_arr = get_bin_centers(bin_edges)
     
-    # one-Gaussian fit
-    try:
-        popt1, pcov1 = curve_fit(skewnorm.pdf, ofe_arr, odf, 
-                                 p0=(0, 0.1, 0.05),
-                                 bounds=([-np.inf, -np.inf, 0.], 
-                                         [np.inf, np.inf, np.inf]))
-        fit1 = skewnorm.pdf(ofe_arr, *popt1)
-    # if skew-normal fit fails, use normal distributions instead
-    except RuntimeError:
-        popt1, pcov1 = curve_fit(norm.pdf, ofe_arr, odf, p0=(0.1, 0.05))
-        fit1 = norm.pdf(ofe_arr, *popt1)
-    # reduced chi-square
-    chisq1 = reduced_chisqare(odf, fit1, N=popt1.shape[0])
-    # KL divergence
-    kldiv1 = kl_divergence(odf, fit1, dx)
+    # inner region fit
+    popt1, pcov1 = curve_fit(norm.pdf, ofe_arr, odf_inner, p0=(0.3, 0.05))
+    fit1 = norm.pdf(ofe_arr, *popt1)
+    # outer region fit
+    popt2, pcov2 = curve_fit(norm.pdf, ofe_arr, odf_outer, p0=(0.1, 0.05))
+    fit2 = norm.pdf(ofe_arr, *popt2)
     
-    # two-Gaussian fit
-    try:
-        popt2, pcov2 = curve_fit(double_skewnorm, ofe_arr, odf, 
-                                 p0=(0, 0.1, 0.05, 0.5, 0, 0.3, 0.05),
-                                 bounds=([-np.inf, -np.inf, 0., 0., -np.inf, -np.inf, 0.], 
-                                         [np.inf, np.inf, np.inf, 1., np.inf, np.inf, np.inf]))
-        fit2 = double_skewnorm(ofe_arr, *popt2)
-        # normalization coefficient of low-alpha component
-        coeff1 = popt2[3]
-        coeff1 = max(coeff1, 1 - coeff1)
-        # difference in means relative to standard deviation
-        delta_mean = np.abs(popt2[5] - popt2[1]) / popt2[2]
-    except RuntimeError:
-        popt2, pcov2 = curve_fit(double_gaussian, ofe_arr, odf, 
-                                 p0=(0.1, 0.05, 0.5, 0.3, 0.05))
-        fit2 = double_gaussian(ofe_arr, *popt2)
-        # normalization coefficient of low-alpha component
-        coeff1 = popt2[2]
-        coeff1 = max(coeff1, 1 - coeff1)
-        # difference in means relative to standard deviation
-        delta_mean = np.abs(popt2[3] - popt2[0]) / popt2[1]
-    # reduced chi-square
-    chisq2 = reduced_chisqare(odf, fit2, N=popt2.shape[0])
-    # KL divergence
-    kldiv2 = kl_divergence(odf, fit2, dx)
-    # difference in chi-square
-    delta_chisq = chisq1 - chisq2
-    # difference in KL divergence
-    delta_kld = kldiv1 - kldiv2
+    # combine standard deviations in quadrature
+    rms_sigma = np.sqrt(0.5 * (popt1[1]**2 + popt2[1]**2))
+    center_diff = popt1[0] - popt2[0]
+    delta_mean = (center_diff / rms_sigma)
     
     # diagnostic plot
     if diagnostic:
         fig, ax = plt.subplots()
-        ax.plot(ofe_arr, odf, label='VICE')
-        ax.plot(ofe_arr, fit1, '--', 
-                label='Single peak fit\n(Chi2=%.01e, KLdiv=%.02f)' % (chisq1, kldiv1))
-        ax.plot(ofe_arr, fit2, '--', 
-                label='Double peak fit\n(Chi2=%.01e, KLdiv=%.02f)' % (chisq2, kldiv2))
-        ax.text(0.68, 0.72, 
-                r'$\Delta\chi^2=%.02f$' % delta_chisq, 
-                transform=ax.transAxes)
-        ax.text(0.68, 0.68,
-                r'$\Delta D_{\rm{KL}}=%.02f$' % delta_kld,
-                transform=ax.transAxes)
-        ax.text(0.68, 0.64, 
-                r'$\Delta\mu=%.02f\sigma_1$' % delta_mean, 
-                transform=ax.transAxes)
-        ax.text(0.68, 0.60, 
-                r'$C_1=%.02f$' % coeff1, 
+        ax.plot(ofe_arr, odf_inner, label='VICE inner region')
+        ax.plot(ofe_arr, odf_outer, label='VICE outer region')
+        ax.plot(ofe_arr, fit1, '--', label='Inner region fit')
+        ax.plot(ofe_arr, fit2, '--', label='Outer region fit')
+        ax.text(0.65, 0.75, 
+                r'$\Delta\mu=%.01f\sigma_{\rm{RMS}}$' % delta_mean, 
                 transform=ax.transAxes)
         ax.legend(loc='upper right')
         ax.set_xlabel('[O/Fe]')
         ax.set_ylabel('PDF')
         figname = '_'.join(output_name.split('/')[1:])
-        plt.savefig(paths.figures / 'bimodality' / 'skewnorm' / ('%s.png' % figname), dpi=140)
+        plt.savefig(paths.figures / 'bimodality' / 'tworegions' / ('%s.png' % figname), dpi=140)
         plt.close()
     
-    return delta_chisq, delta_kld, delta_mean, coeff1
-
-
-def reduced_chisqare(obs, exp, N=1):
-    """
-    Calculate the reduced chi-square statistic.
-    
-    Parameters
-    ----------
-    obs : array-like
-        Observed values
-    exp : array-like
-        Expected values
-    N : int, optional
-        Number of parameters. The default is 1.
-    
-    Returns
-    -------
-    redchisq : float
-        Reduced chi-square value.
-    """
-    # mask zeroes
-    obs = obs[exp > 0]
-    exp = exp[exp > 0]
-    # normalize arrays
-    exp *= np.sum(obs) / np.sum(exp)
-    chisq = chisquare(obs, exp)[0]
-    return chisq / (obs.shape[0] - N)
-
-
-def double_skewnorm(x, a1, loc1, scale1, coeff1, a2, loc2, scale2):
-    """
-    A normalized double skew-normal distribution.
-    
-    Parameters
-    ----------
-    x : float
-        The independent variable.
-    a1 : float
-        Skewness of the first Gaussian component.
-    loc1 : float
-        Center of the first Gaussian.
-    scale1 : float
-        Standard deviation of the first Gaussian.
-    coeff1 : float
-        Normalization coefficient of the first Gaussian. Also defines the 
-        second, which is (1 - coeff1).
-    a2 : float
-        Skewness of the second Gaussian.
-    loc2 : float
-        Center of the second Gaussian.
-    scale2 : float
-        Standard deviation of the second Gaussian.
-    """
-    func1 = skewnorm.pdf(x, a1, loc1, scale1) 
-    func2 = skewnorm.pdf(x, a2, loc2, scale2)
-    return coeff1 * func1 + (1 - coeff1) * func2
-
-
-def double_gaussian(x, loc1, scale1, coeff1, loc2, scale2):
-    """
-    A normalized double Gaussian distribution.
-    
-    Parameters
-    ----------
-    x : float
-        The independent variable.
-    loc1 : float
-        Center of the first Gaussian.
-    scale1 : float
-        Standard deviation of the first Gaussian.
-    coeff1 : float
-        Normalization coefficient of the first Gaussian. Also defines the 
-        second, which is (1 - coeff1).
-    loc2 : float
-        Center of the second Gaussian.
-    scale2 : float
-        Standard deviation of the second Gaussian.
-    """
-    func1 = norm.pdf(x, loc1, scale1) 
-    func2 = norm.pdf(x, loc2, scale2)
-    return coeff1 * func1 + (1 - coeff1) * func2
+    return delta_mean
 
 
 if __name__ == '__main__':
