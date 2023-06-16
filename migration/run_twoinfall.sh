9a9833b7e2d340ceb1a388191093a2aef96e5a46
Liam Dubay <liam.dubay@gmail.com>
2023-06-16 16:50:21 -0400

    Update run_twoinfall.sh

--- migration/run_twoinfall.sh
+++ migration/run_twoinfall.sh
@@ -4,29 +4,32 @@ DTD_LIST=(
 	"powerlaw" 
 	"plateau" 
 	"plateau" 
-	# "exponential" 
+	"exponential" 
 	"exponential" 
 	"prompt"
+    "triple"
 )
 DTD_PARAMS=(
 	"slope=-1.1" 
 	"slope=-1.4" 
 	"width=0.3_slope=-1.1" 
 	"width=1_slope=-1.1" 
-	# "timescale=1.5" 
+	"timescale=1.5" 
 	"timescale=3" 
 	"peak=0.05_stdev=0.015_timescale=3"
+    ""
 )
 DTD_NAMES=(
 	"powerlaw_slope11" 
 	"powerlaw_slope14" 
 	"plateau_width300_slope11" 
 	"plateau_width1000_slope11" 
-	# "exponential_timescale15" 
+	"exponential_timescale15" 
 	"exponential_timescale30" 
 	"prompt_peak050_stdev015_timescale30"
+    "triple_delay040"
 )
-MIGR_LIST=("diffusion" "post-process")
+MIGR_LIST=("diffusion" "post-process" "gaussian")
 EVOL="twoinfall"
 
 for i in ${!DTD_LIST[@]}; do
