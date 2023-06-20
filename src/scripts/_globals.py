a3e5efe39e29d2a6b9f3812285cf3b18e465448e
Liam Dubay <liam.dubay@gmail.com>
2023-06-20 13:15:57 -0400

    revert to default zone width

--- src/scripts/_globals.py
+++ src/scripts/_globals.py
@@ -4,7 +4,7 @@ Global variables for use by multiple plots.
 
 GALR_BINS = [3, 5, 7, 9, 11, 13, 15] # kpc
 ABSZ_BINS = [0, 0.5, 1, 2] # kpc
-ZONE_WIDTH = 1. # kpc
+ZONE_WIDTH = 0.1 # kpc
 DT = 0.01 # Gyr
 END_TIME = 13.2 # Gyr
 
