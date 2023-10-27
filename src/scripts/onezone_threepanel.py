26581b11766f394aeeb202bb915e54351faf4857
Liam Dubay <liam.dubay@gmail.com>
2023-10-27 12:37:10 -0700

    add onezone twopanel, move threepanel script

--- src/scripts/onezone_threepanel.py
+++ src/scripts/onezone_threepanel.py
@@ -1,12 +1,188 @@
 """
-This script generates three plots which demonstrate the effect of changing
-DTD parameters on one-zone model outputs.
+Make a three-panel plot of onezone chemical evolution tracks for the power-law,
+exponential, and plateau DTD models illustrating the effect of changing the
+key parameter for each model.
 """
 
-from onezone_powerlaw_slope import main as plot_powerlaw_slope
-from onezone_exponential_timescale import main as plot_exponential_timescale
-from onezone_plateau_width import main as plot_plateau_width
+import numpy as np
+import matplotlib.pyplot as plt
+import vice
+import paths
+from multizone.src.yields import J21
+from multizone.src import models, dtds
+from _globals import END_TIME, ONEZONE_DEFAULTS, TWO_COLUMN_WIDTH
+from colormaps import paultol
+from track_and_mdf import setup_axes, plot_vice_onezone
+from delay_time_distributions import styles
 
-plot_powerlaw_slope()
-plot_exponential_timescale()
-plot_plateau_width()
+def main():
+    plt.style.use(paths.styles / 'paper.mplstyle')
+    fig = plt.figure(figsize=(TWO_COLUMN_WIDTH, 0.36*TWO_COLUMN_WIDTH))
+    gs = fig.add_gridspec(7, 22, wspace=0.)
+    # subfigs = fig.subfigures(1, 3, wspace=0.)
+    subfigs = [fig.add_subfigure(gs[:,i:i+w]) for i, w in zip((0, 8, 15), (8, 7, 7))]
+    plaw_axs = powerlaw_slope(subfigs[0])
+    exp_axs = exponential_timescale(subfigs[1])
+    plat_axs = plateau_width(subfigs[2])
+    plt.subplots_adjust(bottom=0.13, top=0.98, left=0.16, right=0.98, wspace=0.5)
+    fig.savefig(paths.figures / 'onezone_threepanel.pdf', dpi=300)
+    plt.close()
+
+
+def powerlaw_slope(subfig, slopes=[-0.8, -1.1, -1.4], 
+                   line_styles=['-', '--', ':']):
+    output_dir = paths.data / 'onezone' / 'powerlaw_slope'
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+
+    axs = setup_axes(subfig, title='Power-law DTD', xlim=(-2.1, 0.6))
+    # subfig.gridspec.update(right=1.)
+
+    dt = ONEZONE_DEFAULTS['dt']
+    simtime = np.arange(0, END_TIME + dt, dt)
+    
+    # Exponential DTD for reference
+    dtd = dtds.exponential(timescale=3, tmin=ONEZONE_DEFAULTS['delay'])
+    sz = vice.singlezone(name=str(output_dir / dtd.name),
+                         RIa=dtd,
+                         func=models.insideout(8, dt=dt), 
+                         mode='sfr',
+                         **ONEZONE_DEFAULTS)
+    sz.run(simtime, overwrite=True)
+    plot_vice_onezone(str(output_dir / dtd.name), 
+                      fig=subfig, axs=axs, 
+                      linestyle='-', 
+                      linewidth=2,
+                      color='#bbbbbb', 
+                      label='Exp.\n($\\tau=3$ Gyr)', 
+                      marker_labels=True,
+                      zorder=1)
+
+    for i, slope in enumerate(slopes):
+        dtd = dtds.powerlaw(slope=slope, tmin=ONEZONE_DEFAULTS['delay'])
+        # Run one-zone model
+        sz = vice.singlezone(name=str(output_dir / dtd.name),
+                             RIa=dtd,
+                             func=models.insideout(8, dt=dt), 
+                             mode='sfr',
+                             **ONEZONE_DEFAULTS)
+        sz.run(simtime, overwrite=True)
+        plot_vice_onezone(str(output_dir / dtd.name), 
+                          fig=subfig, axs=axs, 
+                          linestyle=line_styles[i], 
+                          color=styles.plaw['color'], 
+                          label=rf'$\alpha={slope:.1f}$', 
+                          marker_labels=False)
+
+    # Adjust axis limits
+    axs[1].set_ylim(bottom=0)
+    axs[2].set_xlim(left=0)
+    axs[0].legend(frameon=False, loc='lower left')
+
+    return axs
+
+
+def exponential_timescale(subfig, timescales=[6, 3, 1.5], 
+                          line_styles=['-', '--', ':']):
+    output_dir = paths.data / 'onezone' / 'exponential_timescale'
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+
+    axs = setup_axes(subfig, title='Exponential DTD', ylabel=False, xlim=(-2.1, 0.6))
+
+    dt = ONEZONE_DEFAULTS['dt']
+    simtime = np.arange(0, END_TIME + dt, dt)
+
+    for i, timescale in enumerate(timescales):
+        dtd = dtds.exponential(timescale=timescale, 
+                               tmin=ONEZONE_DEFAULTS['delay'])
+        # Run one-zone model
+        sz = vice.singlezone(name=str(output_dir / dtd.name),
+                             RIa=dtd,
+                             func=models.insideout(8, dt=dt), 
+                             mode='sfr',
+                             **ONEZONE_DEFAULTS)
+        sz.run(simtime, overwrite=True)
+        # Plot one-zone models
+        plot_vice_onezone(str(output_dir / dtd.name), 
+                          fig=subfig, axs=axs, 
+                          linestyle=line_styles[i], 
+                          color=styles.exp['color'], 
+                          label=rf'$\tau={timescale:.1f}$ Gyr', 
+                          marker_labels=(i==0))
+
+    # Adjust axis limits
+    axs[1].set_ylim(bottom=0)
+    axs[2].set_xlim(left=0)
+    axs[0].legend(frameon=False, loc='lower left')
+    
+    return axs
+
+
+def plateau_width(subfig, plateau_widths=[1., 0.3, 0.1], 
+                  line_styles=['-', '--', '-.'], slope=-1.1):
+    output_dir = paths.data / 'onezone' / 'plateau_width'
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+
+    dt = ONEZONE_DEFAULTS['dt']
+    delay = ONEZONE_DEFAULTS['delay']
+    simtime = np.arange(0, END_TIME + dt, dt)
+
+    axs = setup_axes(subfig, title='Plateau DTD', ylabel=False, xlim=(-2.1, 0.6))
+
+    # Plot exponentials for reference
+    dtd = dtds.exponential(timescale=3, tmin=delay)
+    sz = vice.singlezone(name=str(output_dir / dtd.name),
+                         RIa=dtd,
+                         func=models.insideout(8, dt=dt), 
+                         mode='sfr',
+                         **ONEZONE_DEFAULTS)
+    sz.run(simtime, overwrite=True)
+    plot_vice_onezone(str(output_dir / dtd.name), 
+                      fig=subfig, axs=axs,
+                      linestyle='-', 
+                      linewidth=2,
+                      color='#bbbbbb', 
+                      label='Exp.\n($\\tau=3$ Gyr)', 
+                      marker_labels=True, 
+                      zorder=1)
+
+    for i, width in enumerate(plateau_widths):
+        dtd = dtds.plateau(width=width, slope=slope, tmin=delay)
+        sz = vice.singlezone(name=str(output_dir / dtd.name),
+                             RIa=dtd,
+                             func=models.insideout(8, dt=dt), 
+                             mode='sfr',
+                             **ONEZONE_DEFAULTS)
+        sz.run(simtime, overwrite=True)
+        plot_vice_onezone(str(output_dir / dtd.name), 
+                          fig=subfig, axs=axs,
+                          linestyle=line_styles[i], 
+                          color=styles.plateau['color'], 
+                          label=r'$W={:1.1f}$ Gyr'.format(width))
+
+    # Plot standard power-law for reference
+    dtd = dtds.powerlaw(slope=slope, tmin=delay)
+    sz = vice.singlezone(name=str(output_dir / dtd.name),
+                         RIa=dtd,
+                         func=models.insideout(8, dt=dt), 
+                         mode='sfr',
+                         **ONEZONE_DEFAULTS)
+    sz.run(simtime, overwrite=True)
+    plot_vice_onezone(str(output_dir / dtd.name), 
+                      fig=subfig, axs=axs, 
+                      linestyle=':', 
+                      color=styles.plaw['color'], 
+                      label='No plateau')
+
+    # Adjust axis limits
+    axs[1].set_ylim(bottom=0)
+    axs[2].set_xlim(left=0)
+    axs[0].legend(frameon=False, loc='lower left')
+    
+    return axs
+
+
+if __name__ == '__main__':
+    main()
--- src/scripts/onezone_twopanel.py
+++ src/scripts/onezone_twopanel.py
@@ -4,8 +4,118 @@ of the minimum SN Ia delay time and the different DTD models on the one-zone
 model outputs.
 """
 
-from onezone_minimum_delay import main as plot_minimum_delay
-from onezone_dtd import main as plot_all
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import vice
+import paths
+from multizone.src.yields import J21
+from multizone.src import models
+from _globals import END_TIME, ONEZONE_DEFAULTS, TWO_COLUMN_WIDTH
+from track_and_mdf import setup_axes, plot_vice_onezone
+from delay_time_distributions import styles
 
-plot_minimum_delay()
-plot_all()
+def main(overwrite=False):
+    plt.style.use(paths.styles / 'paper.mplstyle')
+    fig = plt.figure(figsize=(TWO_COLUMN_WIDTH, 0.54*TWO_COLUMN_WIDTH))
+    gs = fig.add_gridspec(7, 15, wspace=0.)
+    # subfigs = fig.subfigures(1, 3, wspace=0.)
+    subfigs = [fig.add_subfigure(gs[:,i:i+w]) for i, w in zip((0, 8), (8, 7))]
+    minimum_delay(subfigs[0])
+    dtds(subfigs[1])
+    plt.subplots_adjust(bottom=0.09, top=0.98, left=0.11, right=0.98, wspace=0.5)
+    fig.savefig(paths.figures / 'onezone_twopanel.pdf', dpi=300)
+    plt.close()
+    
+
+def minimum_delay(subfig, delays=[0.15, 0.04], line_styles=['--', '-']):
+    output_dir = paths.data / 'onezone' / 'minimum_delay'
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+
+    axs = setup_axes(subfig)
+
+    dt = ONEZONE_DEFAULTS['dt']
+    simtime = np.arange(0, END_TIME + dt, dt)
+    params = ONEZONE_DEFAULTS.copy()
+
+    for delay, ls in zip(delays, line_styles):
+        params['delay'] = delay
+        distributions = [styles.exp_long, styles.plateau, styles.plaw]
+        for i, dtd in enumerate(distributions):
+            if delay == delays[1]:
+                label = dtd['label']
+            else:
+                label = None
+            # Modify mass-loading factor for clarity
+            if i == 0:
+                params['eta'] = 1.
+            else:
+                params['eta'] = ONEZONE_DEFAULTS['eta']
+            name = dtd['func'].name + '_delay{:03d}'.format(int(delay * 1000))
+            sz = vice.singlezone(name=str(output_dir / name),
+                                 RIa=dtd['func'],
+                                 func=models.insideout(8, dt=dt), 
+                                 mode='sfr',
+                                 **params)
+            sz.run(simtime, overwrite=True)
+            plot_vice_onezone(str(output_dir / name), 
+                              fig=subfig, axs=axs,
+                              label=label, 
+                              color=dtd['color'],
+                              linestyle=ls,
+                              zorder=i,
+                              marker_labels=(i==0 and delay==delays[0])
+                              )
+
+
+    axs[0].set_xlim(right=0.7)
+    # Re-scale marginal axis limits
+    axs[1].set_ylim(bottom=0)
+    axs[2].set_xlim(left=0)
+    # Legend
+    handles, labels = axs[0].get_legend_handles_labels()
+    handles += [Line2D([], [], color='k', ls=ls, lw=1) for ls in line_styles]
+    labels += [f'{int(1000*delay)} Myr minimum delay' for delay in delays]
+    axs[0].legend(handles, labels, frameon=False, loc='lower left',
+                  handlelength=1.2)
+    return axs
+
+
+def dtds(subfig):
+    output_dir = paths.data / 'onezone' / 'dtd'
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+    
+    axs = setup_axes(subfig, ylabel=False)
+    
+    dt = ONEZONE_DEFAULTS['dt']
+    simtime = np.arange(0, END_TIME + dt, dt)
+    
+    distributions = [styles.triple, styles.plateau_long, styles.exp, 
+                     styles.plaw, styles.prompt]
+    
+    for i, dtd in enumerate(distributions):
+        sz = vice.singlezone(name=str(output_dir / dtd['func'].name),
+                             RIa=dtd['func'],
+                             func=models.insideout(8, dt=dt), 
+                             mode='sfr',
+                             **ONEZONE_DEFAULTS)
+        sz.run(simtime, overwrite=True)
+        plot_vice_onezone(str(output_dir / dtd['func'].name), 
+                          fig=subfig, axs=axs,
+                          label=dtd['label'], 
+                          color=dtd['color'],
+                          linestyle=dtd['line'],
+                          marker_labels=(i==0),
+                          )
+    
+    # Re-scale marginal axis limits
+    axs[1].set_ylim(bottom=0)
+    axs[2].set_xlim(left=0)
+    axs[0].legend(frameon=False, loc='lower left', handlelength=1.8)
+    return axs
+
+
+if __name__ == '__main__':
+    main()
